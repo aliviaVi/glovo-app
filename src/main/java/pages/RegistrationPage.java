c020e41ac2c887bfeebe091d5c7c0e695f8788d6
alina <alivia0408@gmail.com>
2023-12-28 23:06:16 +0100

    new locators added

--- src/main/java/pages/RegistrationPage.java
+++ src/main/java/pages/RegistrationPage.java
@@ -12,16 +12,18 @@ public class RegistrationPage extends Page {
     private String businessTypeName = "Аптека";
 
     private static final By countryUASelector = By.cssSelector("//*[@id='start-selling']/div/div[1]/form/div[2]/select/option[2]");
-    private static final By citySelector = By.xpath("(//input[@placeholder='Місто'])[1]");
-    private static final By businessName = By.xpath("(//input[@placeholder='Назва бізнесу'])[1]");
-    private static final By name = By.xpath("//*[@id='start-selling']/div/div[1]/form/div[3]/div[1]/input");
-    private static final By surname = By.xpath("(//input[@placeholder='Прізвище'])[1]");
-    private static final By email = By.xpath("(//input[@placeholder='Електронна пошта'])[1]");
+    private static final By citySelector = By.xpath("//form/div/input"); //By.xpath("(//input[@placeholder='Місто'])[1]");
+    private static final By businessName = By.xpath("//form/input"); //By.xpath("(//input[@placeholder='Назва бізнесу'])[1]");
+    private static final By name = By.xpath("//form[@class='signupForm']/div[3]/div[1]/input[1]"); //By.xpath("//*[@id='start-selling']/div/div[1]/form/div[3]/div[1]/input");
+    private static final By surname = By.xpath("//form[@class='signupForm']/div[3]/div[2]/input"); // By.xpath("(//input[@placeholder='Прізвище'])[1]");
+    private static final By email = By.xpath("//form[@class='signupForm']/input[2]"); //By.xpath("(//input[@placeholder='Електронна пошта'])[1]");
     private static final By phoneCodeCountry = By.xpath("(//select[@id='prefix'])[1]");
-    private static final By phone = By.xpath("(//input[@placeholder='Телефон'])[1]");
+    private static final By phone = By.xpath("//form[@class='signupForm']/div[@class='formFlex']/div[2]/input");//By.xpath("(//input[@placeholder='Телефон'])[1]");
     private static final By businessType = By.id("vertical"); //By.xpath("//*[@id='vertical']");
-    private static final By businessTypeFlowers = By.xpath("//*[@id='vertical']/option[5]");
+    private static final By businessTypeFlowers = By.xpath("//form[@class='signupForm']/select[@id='vertical']/option[@value='Flowers']");// By.xpath("//*[@id='vertical']/option[5]");
+    private static final String businessTypeString = "Flowers";
     private static final By movedBusiness = By.id("glovoActs");//"(//option[@value='false'][contains(text(),'Ні')])[1]");
+    private static final String movedBusinessString = "Ні";
     private static final By checkBoxPrivacy  = By.xpath("(//input[@id='privacy'])[1]");
     private static final By submittButton = By.xpath("//*[@id='landing_submitted']");
 
@@ -36,8 +38,8 @@ public class RegistrationPage extends Page {
         $(email).setValue(EMAIL);
         $(phoneCodeCountry).selectOptionByValue(countryCode);
         $(phone).setValue(PHONE);
-        $(businessType).selectOption("Магазин квітів");
-        $(movedBusiness).selectOption("Ні");
+        $(businessType).selectOptionByValue(businessTypeString);
+        $(movedBusiness).selectOption(movedBusinessString);
         $(checkBoxPrivacy).click();
 
     }
